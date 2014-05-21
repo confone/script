224b6e26046c859926e14d7a32e2969efee937d6
peng.shen <peng.shen@indochino.com>
2014-05-20 17:23:55 -0700

    add create folder script

---
+++ node/opt-make.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+mkdir /opt/confone
+mkdir /opt/confone/account
+mkdir /opt/confone/content
+mkdir /opt/confone/security
+mkdir /opt/confone/doc
+mkdir /opt/confone/help
+mkdir /opt/confone/www
+mkdir /opt/confone/account/images
+mkdir /opt/confone/account/releases
+mkdir /opt/confone/account/logs
+chmod 777 /opt/confone/account/logs
+mkdir /opt/confone/content/images
+mkdir /opt/confone/content/releases
+mkdir /opt/confone/content/logs
+chmod 777 /opt/confone/content/logs
+mkdir /opt/confone/security/releases
+mkdir /opt/confone/security/logs
+chmod 777 /opt/confone/security/logs
+mkdir /opt/confone/doc/releases
+mkdir /opt/confone/doc/logs
+chmod 777 /opt/confone/doc/logs
+mkdir /opt/confone/help/releases
+mkdir /opt/confone/help/logs
+chmod 777 /opt/confone/help/logs
+mkdir /opt/confone/www/releases
+mkdir /opt/confone/www/logs
+chmod 777 /opt/confone/www
